b22e993bc65872b5ec281c1e5b3342662c5ae0d5
Lykhosherstyy <evgeniy.arximed@gmail.com>
2015-10-15 20:53:53 +0300

    second commit

--- src/js/partials/slider.js
+++ src/js/partials/slider.js
@@ -1,55 +1,53 @@
-
 $(document).ready(function(){
- function MySlider(){
-
-  var slideWrap = $('.slide-wrap');
-  var nextLink = $('.nextbutton');
-  var prevLink = $('.prewbutton');
-  var is_animate = false;
-  var slideWidth = $('.slide-item').innerWidth();
-  var scrollSlider = slideWrap.position().left - slideWidth;
-
-   /* Клик по ссылке на следующий слайд */
-    nextLink.click(function(e){
-       e.preventDefault();
-   if(!slideWrap.is(':animated')) {
-    slideWrap.animate({left: scrollSlider}, 500, function(){
-     slideWrap
-      .find('.slide-item:first')
-      .appendTo(slideWrap)
-      .parent()
-      .css({'left': 0});
-    });
-   }
-  });
-		
-  /* Клик по ссылке на предыдующий слайд */
-    prevLink.click(function(e){
-      e.preventDefault();
-   if(!slideWrap.is(':animated')) {
-    slideWrap
-     .css({'left': scrollSlider})
-     .find('.slide-item:last')
-     .prependTo(slideWrap)
-     .parent()
-     .animate({left: 0}, 500);
-   }
-  });
-
-  function autoplay(){
-   if(!is_animate){
-    is_animate = true;
-    slideWrap.animate({left: scrollSlider}, 3500, function(){
-     slideWrap
-      .find('.slide-item:first')
-      .appendTo(slideWrap)
-      .parent()
-      .css({'left': 0});
-     is_animate = false;
-    });
-   }
+  
+  //// ---> Проверка на существование элемента на странице
+  $.fn.exists = function() {
+     return $(this).length;
   }
- }
- 
-MySlider();
+
+  //// ---> Слайдер
+  $(function(){
+    if($('.slider').exists()) {
+
+      $('.slider').each(function(){
+
+        var slider = $(this);
+
+        var slideWrap = slider.find('.slide-wrap'),
+            nextLink = slider.find('.next-slide'),
+            prevLink = slider.find('.prev-slide'),
+            slideWidth = slider.find('.slide-item').outerWidth(),
+            scrollSlider = slideWrap.position().left - slideWidth;
+
+        /* Клик по ссылке на следующий слайд */
+        nextLink.click(function(){
+          if(!slideWrap.is(':animated')) {
+            slideWrap.animate({left: scrollSlider}, 500, function(){
+              slideWrap
+                .find('.slide-item:first')
+                .appendTo(slideWrap)
+                .parent()
+                .css({'left': 0});
+              });
+            }
+         });
+
+        /* Клик по ссылке на предыдующий слайд */
+        prevLink.click(function(){
+        if(!slideWrap.is(':animated')) {
+          slideWrap
+            .css({'left': scrollSlider})
+            .find('.slide-item:last')
+            .prependTo(slideWrap)
+            .parent()
+            .animate({left: 0}, 500);
+          }
+        });
+
+      });
+
+    }
+
+  });
+
 });
